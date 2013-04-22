af3b7b1789a7b99301e12a15052aa055c8840dee
mytory <mytory@gmail.com>
2013-04-23 03:20:10 +0900

    평소 watch는 평범한 css로 뽑히게 함.

---
+++ wp-content/themes/left21webmaster/watch-compress.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sass --watch --style compressed style.sass:style.css &
+sass --watch --style compressed style-ie.sass:style-ie.css &
--- wp-content/themes/left21webmaster/watch.sh
+++ wp-content/themes/left21webmaster/watch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sass --watch --style compressed style.sass:style.css &
-sass --watch --style compressed style-ie.sass:style-ie.css &
+sass --watch style.sass:style.css &
+sass --watch style-ie.sass:style-ie.css &
