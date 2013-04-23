940201a7bfab814dfd8844542da81c8d162611a8
mytory <mytory@gmail.com>
2013-04-23 12:13:18 +0900

    ie 맞추기
    사이드바 레이아웃 깨지는 거 정리
    ie7 글꼴 맑은 고딕

--- wp-content/themes/left21webmaster/sidebar.php
+++ wp-content/themes/left21webmaster/sidebar.php
@@ -2,13 +2,19 @@
 <aside class="sidebar">
 	<div class="gw">
 		<div class="g one-third sidebar__column">
-			<ul class="sidebar__one"><? dynamic_sidebar( 'sidebar-left' ); ?></ul>		
+			<div class="g__inner">
+				<ul class="sidebar__one"><? dynamic_sidebar( 'sidebar-left' ); ?></ul>
+			</div>
 		</div>
 		<div class="g one-third sidebar__column">
-			<ul class="sidebar__one"><? dynamic_sidebar( 'sidebar-center' ); ?></ul>
+			<div class="g__inner">
+				<ul class="sidebar__one"><? dynamic_sidebar( 'sidebar-center' ); ?></ul>
+			</div>
 		</div>
 		<div class="g one-third sidebar__column">
-			<ul class="sidebar__one"><? dynamic_sidebar( 'sidebar-right' ); ?></ul>
+			<div class="g__inner">
+				<ul class="sidebar__one"><? dynamic_sidebar( 'sidebar-right' ); ?></ul>
+			</div>
 		</div>
 	</div>
 </aside>
