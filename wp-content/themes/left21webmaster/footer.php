321db551dbcda65623576f4430ddbd7ea4072e1b
mytory <mytory@gmail.com>
2013-04-24 22:51:54 +0900

    footer에 기술 스택을 밝힘.

--- wp-content/themes/left21webmaster/footer.php
+++ wp-content/themes/left21webmaster/footer.php
@@ -1,6 +1,10 @@
 <?get_sidebar()?>
 <footer class="site-footer">
 	<a href="<?=home_url()?>">ⓒ레프트21 웹마스터</a>
+	<br>
+	<a href="http://inuitcss.com/">inuit.css (v4.1.5)</a> | 
+	<a href="http://icomoon.io/">IcoMoon</a> | 
+	<a href="https://github.com/left21wm/webmaster.left21.com">Code on GitHub</a>
 </footer>
 </div><!-- .wrapper -->
 <?php wp_footer(); ?>
