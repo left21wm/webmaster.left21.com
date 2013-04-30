b444256876aa5fad8472e7ac9c21b4dc9b36c296
mytory <mytory@gmail.com>
2013-04-30 20:43:40 +0900

    글꼴 한글 이름 추가. 첫 화면 목록 줄간격 더 띄움. sass watch할 때 compressed하도록.
    ass watch할 때 compressed하도록. On branch master

--- wp-content/themes/left21webmaster/index.php
+++ wp-content/themes/left21webmaster/index.php
@@ -4,7 +4,7 @@
 	<? if ( have_posts() ) : ?>
 		<ul>
 			<? while ( have_posts() ) : the_post(); ?>
-			<li>
+			<li class="home-li">
 				<a href="<?the_permalink()?>"><?the_title()?></a>
 				<small><?=get_the_date()?></small>
 			</li>
